920e32146a27898be0158c97a0e70f67cdb30302
456sasa <2651586469@qq.com>
2021-07-28 17:11:56 +0800

    git add

--- requests/binary.py
+++ requests/binary.py
@@ -4,4 +4,4 @@ r = requests.get('https://img2.baidu.com/it/u=2220420611,1081221264&fm=26&fmt=au
 print(r.content)
 # 将图片保存到本地
 with open('requests/text.png','wb') as f:
-    f.write(r.content)
+    f.write(r.content) 
